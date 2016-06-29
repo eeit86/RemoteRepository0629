d3706de292d4706213230fcd2061852b6688bec7
孫睿辰 <@EEIT>
2016-06-29 16:23:53 +0800

    123

--- ProjVersionControl/src/main/java/controller/Servlet1.java
+++ ProjVersionControl/src/main/java/controller/Servlet1.java
@@ -23,7 +23,8 @@ public class Servlet1 extends HttpServlet {
 		System.out.println("Servlet2");
 
 		System.out.println("Hello Nigger @@@！！");
-		
+		System.out.println("Hello Nigger @@@！！");		
+		System.out.println("Hello Nigger @@@！！");
 
 
 
